cac66658f46d026e71f079d4dc5228439f021190
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2023-10-26 23:52:22 +0600

    show square and added click each square

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,58 @@
-import React, {useState} from 'react';
-import {Item} from "../types";
+import {useState} from 'react';
+import {Item} from '../types';
+import './App.css';
 
-const App = () => {
-    const [items, setItems] = useState<Item[]>([]);
+const createItems = () => {
+  const squares = [];
+  const randomNumber = Math.floor(Math.random() * 36);
 
-    const createItems = () => {
-        const squares = [...items];
-        const item: Item = {
-            hasItem: false,
-            clicked: false
-        };
-
-        for (let i = 0; i < 36; i++){
-            squares.push(item);
-        }
-
-        console.log(squares);
-
+  for (let i = 0; i < 36; i++) {
+    const item: Item = {
+      hasItem: false,
+      clicked: false
     };
 
-    createItems();
+    squares.push(item);
+  }
 
+  squares[randomNumber].hasItem = true;
+  return squares;
+};
 
-    return (
-        <div>
-            
+const App = () => {
+  const [items, setItems] = useState<Item[]>(createItems());
+
+  const clickSquare = (item: Item) => {
+    setItems((prevState): Item[] => {
+      prevState.map((itemState) => {
+        if (item === itemState){
+          itemState.clicked = true;
+        }
+      });
+
+      return prevState;
+    });
+  };
+
+  return (
+    <div className="container">
+      <div className="gamePlay">
+        <div className="squaresContainer">
+          {
+            items.map((item, index) => {
+              return (
+                <div key={index} className={item.clicked ? "open" : "square"} onClick={() => clickSquare(item)}>
+                  {item.hasItem ? 'here' : null}
+                </div>
+              );
+            })
+          }
         </div>
-    );
+        <p>Tries: 0</p>
+        <button>Reset</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
